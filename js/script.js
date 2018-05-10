a649a75fb0bee7c9186faae668e88bfd9b9aa3d1
abdullahi mohamed <abdullahimoh25@gmail.com>
2018-05-10 15:05:27 +0300

    added address button

--- js/script.js
+++ js/script.js
@@ -3,41 +3,75 @@ function Contact(first,last,email){
   this.firstName=first;
   this.lastName=last;
   this.email=email;
+  this.addresses=[];
+}
+function Address(street,city,county){
+  this.street=street;
+  this.city=city;
+  this.county=county;
 }
 
 Contact.prototype.fullName =function (){
-  return this.firstName + "" + this.lastName + this.email;
+  return this.firstName + "" + this.lastName + this.email + this.addresses;
 }
+Address.prototype.fullAddress=function(){
+  return this.street + "," + this.city + "," + this.county;
+}
+
 
 //user interface
 $(document).ready(function() {
-    $("form#new-contact").submit(function(event) {
-      event.preventDefault();
 
+  $("#add-address").click(function() {
+    $("#new-addresses").append('<div class="new-address">' +
+                                 '<div class="form-group">' +
+                                   '<label for="new-street">Street</label>' +
+                                   '<input type="text" class="form-control new-street">' +
+                                 '</div>' +
+                                 '<div class="form-group">' +
+                                   '<label for="new-city">City</label>' +
+                                   '<input type="text" class="form-control new-city">' +
+                                 '</div>' +
+                                 '<div class="form-group">' +
+                                   '<label for="new-county">County</label>' +
+                                   '<input type="text" class="form-control new-county">' +
+                                 '</div>' +
+                               '</div>');
+  });
 
+  $("form#new-contact").submit(function(event) {
+    event.preventDefault();
 
+    var inputtedFirstName = $("input#new-first-name").val();
+    var inputtedLastName = $("input#new-last-name").val();
+    var newContact = new Contact(inputtedFirstName, inputtedLastName);
 
-      var inputtedFirstName = $("input#new-first-name").val();
-      var inputtedLastName = $("input#new-last-name").val();
-      var inputtedemail=$("input#new-email").val();
-
-      var newContact = new Contact(inputtedFirstName, inputtedLastName,inputtedemail);
+    $(".new-address").each(function() {
+      var inputtedStreet = $(this).find("input.new-street").val();
+      var inputtedCity = $(this).find("input.new-city").val();
+      var inputtedCounty = $(this).find("input.new-county").val();
+      var newAddress = new Address(inputtedStreet, inputtedCity, inputtedCounty)
+      newContact.addresses.push(newAddress)
+    });
 
     $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
 
-      $("input#new-first-name").val("");
-      $("input#new-last-name").val("");
-      $("input#email").val("");
-
-      $(".contact").last().click(function() {
-     $("#show-contact").show();
-     $("#show-contact h2").text(newContact.firstName);
-     $(".first-name").text(newContact.firstName);
-     $(".last-name").text(newContact.lastName);
-      $(".email").text(newContact.email);
-
-   });
-
-
+    $(".contact").last().click(function() {
+      $("#show-contact").show();
+      $("#show-contact h2").text(newContact.fullName());
+      $(".first-name").text(newContact.firstName);
+      $(".last-name").text(newContact.lastName);
+      $("ul#addresses").text("");
+      newContact.addresses.forEach(function(address) {
+        $("ul#addresses").append("<li>" + newAddress.fullAddress() + "</li>");
+      });
     });
+
+    $("input#new-first-name").val("");
+    $("input#new-last-name").val("");
+    $("input.new-street").val("");
+    $("input.new-city").val("");
+    $("input.new-county").val("");
+
   });
+});
